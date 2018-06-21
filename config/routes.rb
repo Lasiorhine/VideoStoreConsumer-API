93bab4a1652de97e91311991cac2bed27caaf030
Brittany Jones <brittany.r.jones@hotmail.com>
2018-06-21 15:01:58 -0700

    functionality to post movie to library, not fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :movies, only: [:index, :show], param: :title
 
+  post "/movies", to: "movies#create", param: [:title, :release_date, :image_url, :external_id]
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
